c52233b2c664acf8ab67c603453dbd1455d5aeb8
lilyliuce <lilyliuce@gmail.com>
2019-06-04 17:00:14 +0900

    add merge sort

---
+++ python/merge_sort.py
@@ -0,0 +1,37 @@
+import math
+import random
+
+
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+
+        left_arr = arr[:mid]
+        right_arr = arr[mid:]
+
+        merge_sort(left_arr)
+        merge_sort(right_arr)
+
+        left_arr.append(math.inf)
+        right_arr.append(math.inf)
+
+        i = 0
+        j = 0
+
+        for k in range(0, len(arr)):
+            if left_arr[i] < right_arr[j]:
+                arr[k] = left_arr[i]
+                i = i + 1
+            else:
+                arr[k] = right_arr[j]
+                j = j + 1
+
+    return arr
+
+
+if __name__ == "__main__":
+    test = random.sample(range(1, 100), 10)
+
+    test = merge_sort(test)
+
+    print(test)
